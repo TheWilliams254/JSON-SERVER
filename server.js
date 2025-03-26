c460618cb4b1794e5e2623b45c82e49103fdb1a2
william <williamwambugu663@gmail.com>
2025-03-26 16:49:44 +0300

    Fix CORS issue

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
+server.use(cors({
+  origin: "*", // ✅ Allow all domains
+  methods: ["GET", "POST", "PATCH", "DELETE"], //allowing the methods
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 server.use(middlewares);
 server.use('/api', router);
 
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
-
-  
