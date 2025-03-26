859e6a82937943a2a4645ff4fdb17c98cba421b6
william <williamwambugu663@gmail.com>
2025-03-26 16:42:05 +0300

    adding cors middleware

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const cors = require('cors');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('/api', router)
+server.use(cors()); // ✅ Enable CORS for all routes
+server.use(middlewares);
+server.use('/api', router);
+
 server.listen(process.env.PORT || 3000, () => {
-    console.log('JSON Server is running on port 3000');
-  });
+  console.log('JSON Server is running');
+});
+
   
